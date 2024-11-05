4d12978b8e53764bf7a2093b08c7a69aa91eb8f7
salmanabdullahfahim <salmanabdullah0304@gmail.com>
2024-11-05 12:22:14 +0600

    :sparkles: retrieve all admin data from db

---
+++ src/app/modules/Admin/admin.controller.ts
@@ -0,0 +1,24 @@
+import { Request, Response } from "express";
+import { AdminService } from "./admin.service";
+
+const getAllAdmin = async (req: Request, res: Response) => {
+  try {
+    const result = await AdminService.getAllAdmin();
+
+    res.status(200).json({
+      success: true,
+      message: "All admin retrieved successfully",
+      data: result,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error?.name || "Something went wrong",
+      error: error,
+    });
+  }
+};
+
+export const AdminController = {
+  getAllAdmin,
+};
