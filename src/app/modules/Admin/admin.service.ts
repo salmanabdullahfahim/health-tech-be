bac982cff5434f9b4831d7511c9c3e321fe974ca
salmanabdullahfahim <salmanabdullah0304@gmail.com>
2024-11-05 13:16:38 +0600

    🎗️optimize the searching fn

--- src/app/modules/Admin/admin.service.ts
+++ src/app/modules/Admin/admin.service.ts
@@ -1,24 +1,25 @@
-import { PrismaClient } from "@prisma/client";
+import { Prisma, PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 const getAllAdmin = async (params: any) => {
+  const andConditions: Prisma.AdminWhereInput[] = [];
+
+  if (params.searchTerm) {
+    andConditions.push({
+      OR: ["name", "email"].map((field) => ({
+        [field]: {
+          contains: params.searchTerm,
+          mode: "insensitive",
+        },
+      })),
+    });
+  }
+
+  const whereConditions: Prisma.AdminWhereInput = {
+    AND: andConditions,
+  };
   const result = await prisma.admin.findMany({
-    where: {
-      OR: [
-        {
-          name: {
-            contains: params.searchTerm,
-            mode: "insensitive",
-          },
-        },
-        {
-          email: {
-            contains: params.searchTerm,
-            mode: "insensitive",
-          },
-        },
-      ],
-    },
+    where: whereConditions,
   });
 
   return result;
